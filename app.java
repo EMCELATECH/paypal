fef090c6c2fe7865589ed1492bdcad139a95dbb4
Chukwuemeka Egbo <125903307+emekaegbo@users.noreply.github.com>
2023-03-05 10:03:02 -0800

    Update app.java

--- app.java
+++ app.java
@@ -1,9 +1,8 @@
 we manage and support;
-web app,
-we develop java application,
-.net application,
-nodejs application,
-python applications,
-emeka is doing well,
-trying to decode git,
-bug fixing branch
+web app
+we develop java application
+.net application
+nodejs application
+python applications
+emeka is doing well
+trying to decode git
